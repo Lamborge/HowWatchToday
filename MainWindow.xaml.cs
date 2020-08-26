222ce789a3a770bcd13195eb81ae146b61a83556
Lamborge <maxpopelov@gmail.com>
2020-08-26 17:51:08 +0300

    programm can show only name of film

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,9 @@ namespace WhatWatchToday
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        CQ dom = CQ.CreateFromUrl("https://www.ivi.ru/collections/best-movies/page4");
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +31,31 @@ namespace WhatWatchToday
 
         private void MainButton_Click(object sender, RoutedEventArgs e)
         {
+            //CQ dom = CQ.CreateFromUrl("https://www.ivi.ru/collections/best-movies/page4");
+            Random rnd = new Random();
+
+            string[] list_films = new string[29];
+
+            int i = 0;
+            foreach (IDomObject item in dom.Find(".nbl-slimPosterBlock__title"))
+            {
+                if (i >= 29)
+                {
+                    break;
+                }
+                else
+                {
+
+                    list_films[i] = item.TextContent;
+                    i++;
+
+                }
+            }
+
+            //MessageBox.Show(list_films[rnd.Next(0, list_films.Length)]);
+
+            int num_film = rnd.Next(0, list_films.Length);
+            MessageBox.Show(list_films[num_film]);
 
         }
     }
