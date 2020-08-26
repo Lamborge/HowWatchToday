93f35324560fa6e98adb1b6f515dce26da8e6566
Lamborge <maxpopelov@gmail.com>
2020-08-26 16:31:51 +0300

    added button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CsQuery;
 
 namespace WhatWatchToday
 {
@@ -23,6 +24,11 @@ namespace WhatWatchToday
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
